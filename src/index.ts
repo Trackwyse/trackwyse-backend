0c9e2d82ef75200b9235273b99e7861dfd076643
JS <49812749+JS00001@users.noreply.github.com>
2022-11-30 19:42:00 +0000

    Router groups

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,18 @@ import db from './db';
 
 const app = express();
 
-const init = async () => {
+const start = async () => {
   app.use(cors());
   app.use(express.json());
   app.use(morganLogger);
 
   await db.connect();
-};
 
-const start = async () => {
-  await init();
+  const authRouter = express.Router();
+  const apiRouter = express.Router();
+
+  app.use('/auth/v1', authRouter);
+  app.use('/api/v1', apiRouter);
 
   app.listen(config.Port, () => {
     logger.info(`Application started in mode: ${config.NodeEnv}`);
