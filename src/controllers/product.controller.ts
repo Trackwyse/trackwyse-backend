fd9c4f12996a502af3e8c7af943a2cce66597598
JS <49812749+JS00001@users.noreply.github.com>
2022-12-13 01:16:06 +0000

    Fix vulnerability

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -52,7 +52,9 @@ const createLabel = async (req: express.Request, res: express.Response) => {
 const getLabels = async (req: express.Request, res: express.Response) => {
   const labels = req.user.labels;
 
-  if (!labels) {
+  console.log(labels);
+
+  if (labels.length === 0 || !labels) {
     return res.status(200).json({
       error: false,
       message: 'No labels found',
