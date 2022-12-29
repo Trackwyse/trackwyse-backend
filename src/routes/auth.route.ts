1cb0011f2f75bcae315dc5d000b6ec2be131e716
JS <49812749+JS00001@users.noreply.github.com>
2022-12-29 10:58:56 -0500

    Fix me bug

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -10,7 +10,6 @@ authRouter.post("/forgot", authController.forgot);
 authRouter.post("/register", authController.register);
 authRouter.post("/checkEmail", authController.checkEmail);
 
-authRouter.get("/me", authMiddleware.authenticateAccessToken, authController.me);
 authRouter.post("/refresh", authMiddleware.authenticateRefreshToken, authController.refresh);
 authRouter.post("/logout", authMiddleware.authenticateAccessToken, authController.logout);
 authRouter.post("/verify", authMiddleware.authenticateUnverifiedAccessToken, authController.verify);
