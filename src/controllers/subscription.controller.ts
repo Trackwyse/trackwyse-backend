5e545cc4933b181a726ea3c70d6093dcc72e77c5
JS <49812749+JS00001@users.noreply.github.com>
2023-01-03 16:22:52 -0500

    idek

--- src/controllers/subscription.controller.ts
+++ src/controllers/subscription.controller.ts
@@ -54,6 +54,9 @@ const getSubscription = async (req: express.Request, res: express.Response) => {
     - user: SanitizedUser
 */
 const createSubscription = async (req: express.Request, res: express.Response) => {
+  console.log(JSON.stringify(req, null, 2));
+  console.log(JSON.stringify(req.body, null, 2));
+  console.log(JSON.stringify(req.user, null, 2));
   const { receipt } = req.body;
 
   if (!receipt) {
