ae3948f860fbf2ec6f69b75991c4ef67128dfa07
JS <49812749+JS00001@users.noreply.github.com>
2023-01-17 22:48:47 -0500

    remove transactions

--- src/controllers/transactions.controller.ts
+++ src/controllers/transactions.controller.ts
@@ -8,7 +8,6 @@
 import express from "express";
 
 import User from "@/models/user.model";
-import Transaction from "@/models/transaction.model";
 
 const getTransactions = async (req: express.Request, res: express.Response) => {
   const user = await User.findById(req.user.id);
@@ -19,18 +18,6 @@ const getTransactions = async (req: express.Request, res: express.Response) => {
       message: "User not found",
     });
   }
-
-  const transactions = user.transactions;
-
-  const transactionDocuments = await Transaction.find({
-    _id: { $in: transactions },
-  });
-
-  return res.status(200).json({
-    error: false,
-    message: "Transactions retrieved successfully",
-    transactions: transactionDocuments,
-  });
 };
 
 export default {
