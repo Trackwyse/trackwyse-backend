9870a70c9b99b5ff8ee4ec3164bb50f0e9225da7
JS <49812749+JS00001@users.noreply.github.com>
2023-02-09 21:48:29 -0500

    Add express limits

--- src/index.ts
+++ src/index.ts
@@ -38,8 +38,8 @@ const startServer = async () => {
   app.use(cors());
   app.use(helmet());
   app.use(morganLogger);
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json({ limit: "200mb" }));
+  app.use(express.urlencoded({ extended: true, limit: "200mb" }));
   app.use(mongoSanitize());
 
   await db.connect();
